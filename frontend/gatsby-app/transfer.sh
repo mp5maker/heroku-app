f2aa39a1d9ec26f012fe13db8b16a4355640c824
Shabuktagin Photon Khan <khan.photon@gmail.com>
2019-04-27 20:27:32 +0600

    Added transfer files

---
+++ frontend/gatsby-app/transfer.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cp -R /home/photon/Downloads/Codes/heroku-app/frontend/gatsby-app/src/* /home/photon/Downloads/Codes/heroku-django-gatsby-react/src/
---
+++ frontend/react-app/transfer.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cp -R /home/photon/Downloads/Codes/heroku-app/frontend/react-app/src/* /home/photon/Downloads/Codes/heroku-django-webpack-react/src/
